92441e5844a4365f53fa3e26544af94bfb972a15
Moisés Antonio Martínez Martínez <SMIS071221@ugb.edu.sv>
2021-08-13 11:09:05 -0600

    Agregar archivos de proyecto.

---
+++ PracticaEjercicio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseTeorica
+{
+    class PracticaEjercicio
+    {
+        static void Main(string[] args)
+        {
+
+            //Declarar y llenar array
+            string[] Estudiantes = new string[5];
+            string[] genero= new string [5];
+            int[] edad = new int[5];
+
+            int femenino = 0;
+            int masculino = 0;
+            //Solicitar nombre
+            //Solocitar genero
+            //Solicitar edad
+                for (int i = 0; i < Estudiantes.Length; i++)
+            {
+                Console.WriteLine("Escriba el nombre del estudiante ");
+                Console.Write("Nombre {0}: ", i);
+                Estudiantes[i] = Console.ReadLine();
+                Console.WriteLine("Seleccione el genero F/M ");
+                Console.Write("Generó: ");
+                genero[i] = Console.ReadLine();
+                Console.WriteLine("Ingrese la edad del estudiante");
+                Console.Write("Edad: ");
+                edad[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(Estudiantes[i], genero[i], edad[i]);
+            }
+
+            Console.WriteLine("Datos de los estudiantes: ");
+            for (int i = 0; i < Estudiantes.Length; i++) 
+            {
+                Console.WriteLine("Nombre: {0}  l  Genero: {1} l Edad: {2}", Estudiantes[i], genero[i], edad[i]);
+
+                if (genero[i]== "F")
+                {
+
+                    femenino += 1;
+
+                }
+
+                if (genero[i]=="M")
+                {
+                    masculino += 1;
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseTeorica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            loops();
+
+            Console.ReadLine();
+        }
+
+        public static void loops()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            int i = 0;
+            Console.WriteLine("IMPRIME ELEMENTOS CON WHILE");
+            while (10 < numbers.Length) ;
+            {
+                Console.WriteLine(numbers[i]);
+                i = i + 1;
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON DO-WHILE");
+            int inc = 0;
+            do
+            {
+                Console.WriteLine(numbers[inc]);
+                inc++;
+            } while (inc < numbers.Length);
+
+            Console.WriteLine("IMPRIMA ELEMENTOS CON FOR");
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine("Posicion {0}: {1}", j, numbers[j]);
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON FOREACH");
+            foreach (int item in numbers)
+
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+    }
+}
